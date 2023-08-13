46357806eb234abdae7c460bf9892209ff9fa7c2
Felipe Sacramento <mzssacramento@gmail.com>
2023-08-12 22:12:44 -0300

    progresso: 11 de 19

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,6 @@
 class CaixaDaLanchonete {
     constructor() {
-        const produtos = [
+        this.produtos = [
             { codigo: 'cafe', descricao: 'Café', valor: 3.00 },
             { codigo: 'chantily', descricao: 'Chantily (extra do Café)', valor: 1.50 },
             { codigo: 'suco', descricao: 'Suco Natural', valor: 6.20 },
@@ -12,22 +12,44 @@ class CaixaDaLanchonete {
         ];
     }
 
-    //                   [formadepagamento, [codigo, quantidade]]
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        let metodoDePagamentoMinusculo = metodoDePagamento.toLowerCase();
-
-        if (itens.length === 0) {
-            return "Não há itens no carrinho de compra!";
-        }
-
-        if (metodoDePagamentoMinusculo !== 'dinheiro' && metodoDePagamentoMinusculo !== 'credito' && metodoDePagamentoMinusculo !== 'debito') {
-            return "Forma de pagamento inválida!";
-        }
-
-        return "";
+    //Solução que achei no stackoverflow: https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript
+    formatarValor(valor) {
+        return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     }
 
 
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        let metodoDePagamentoMinusculo = metodoDePagamento.toLowerCase();
+    
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!";
+        }
+    
+        if (metodoDePagamentoMinusculo !== 'dinheiro' && metodoDePagamentoMinusculo !== 'credito' && metodoDePagamentoMinusculo !== 'debito') {
+            return "Forma de pagamento inválida!";
+        }
+    
+        let valorDoPedido = 0;
+        for (const item of itens) {
+            const [codigo, quantidade] = item.split(','); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+            const produto = this.produtos.find(p => p.codigo === codigo);
+            if (produto) {
+                valorDoPedido += produto.valor * parseInt(quantidade);
+            }
+        }
+    
+        if (metodoDePagamentoMinusculo === 'dinheiro') {
+            return this.formatarValor(valorDoPedido - valorDoPedido * 0.05);
+        }
+        if (metodoDePagamentoMinusculo === 'credito') {
+            return this.formatarValor(valorDoPedido + valorDoPedido * 0.03);
+        }
+        if (metodoDePagamentoMinusculo === 'debito') {
+            return this.formatarValor(valorDoPedido);
+        }
+    
+        return "Forma de pagamento inválida!";
+    }
 }
 
 export { CaixaDaLanchonete };
