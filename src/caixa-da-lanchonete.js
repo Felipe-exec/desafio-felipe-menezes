f23ef15ecae56d6899e5001fcc5c2bfe9f3f6b99
Felipe Sacramento <mzssacramento@gmail.com>
2023-08-13 08:48:20 -0300

    progresso: 19 de 19, completo!

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -12,11 +12,11 @@ class CaixaDaLanchonete {
         ];
     }
 
-    //Solução que achei no stackoverflow: https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript
-    formatarValor(valor) {
-        return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    }
-
+    //https://pt.stackoverflow.com/questions/181922/formatar-moeda-brasileira-em-javascript
+    //Uma solução alternativa (e mais fácil) ao toFixed() e que poderia ser usado em caso real! Veja:
+    //formatarValor(valor) {
+    //    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    //}
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         let metodoDePagamentoMinusculo = metodoDePagamento.toLowerCase();
@@ -31,12 +31,28 @@ class CaixaDaLanchonete {
     
         let valorDoPedido = 0;
         let pedido = "";
+        let chantilyPedido = false;
+        let queijoPedido = false;
         for (const item of itens) {
             const [codigo, quantidade] = item.split(',');
-            const produto = this.produtos.find(p => p.codigo === codigo);
-            if (produto) {
+            const produto = this.produtos.find(p => p.codigo === codigo); //undefined é considerado um valor truthy em JavaScript. Loucura, não?
+
+            if (quantidade == 0) {
+                return "Quantidade inválida!";
+            }
+
+            if (produto !== undefined) {
                 valorDoPedido += produto.valor * parseInt(quantidade);
                 pedido += codigo;
+
+                if (codigo === "chantily") {
+                    chantilyPedido = true;
+                }
+                if (codigo === "queijo") {
+                    queijoPedido = true;
+                }
+            } else {
+                return "Item inválido!";
             }
         }
         
@@ -44,14 +60,23 @@ class CaixaDaLanchonete {
             return "Item extra não pode ser pedido sem o principal";
         }
 
+        if (chantilyPedido == true && !pedido.includes("cafe") || queijoPedido == true && !pedido.includes("sanduiche")) {
+            return "Item extra não pode ser pedido sem o principal";
+        }
+
         if (metodoDePagamentoMinusculo === 'dinheiro') {
-            return this.formatarValor(valorDoPedido - valorDoPedido * 0.05);
+            const pagarEmDinheiro = valorDoPedido - valorDoPedido * 0.05;
+            return "R$ " + pagarEmDinheiro.toFixed(2).replace('.', ',');
         }
+
         if (metodoDePagamentoMinusculo === 'credito') {
-            return this.formatarValor(valorDoPedido + valorDoPedido * 0.03);
+            const pagarEmCredito = valorDoPedido + valorDoPedido * 0.03;
+            return "R$ " + pagarEmCredito.toFixed(2).replace('.', ',');
         }
+
         if (metodoDePagamentoMinusculo === 'debito') {
-            return this.formatarValor(valorDoPedido);
+            const pagarEmDebito = valorDoPedido;
+            return "R$ " + pagarEmDebito.toFixed(2).replace('.', ',');
         }
     
         return "Forma de pagamento inválida!";
